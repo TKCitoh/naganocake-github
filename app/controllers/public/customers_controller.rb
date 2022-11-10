7500f5cdc97da1de0f9f3e3198f652d4f2697651
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-11-10 08:26:24 +0000

    【add】顧客登録除法編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,27 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customers_my_page_path(current_customer.id)
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
