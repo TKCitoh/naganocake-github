c72fdc9dd2000efa742674d6d62bff714be849d1
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-11-17 01:44:16 +0000

    【add】退会処理

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @item = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,6 +17,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+    @customer = current_customer
   end
 
   private
