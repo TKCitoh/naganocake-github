77dc545896ed0f4dcff074f8c8600ff50894220d
TKCitoh <tkci10.pooh@gmail.com>
2022-11-23 08:04:10 +0000

    注文履歴一覧途中まで【注文機能は未実装】

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,12 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+  　redirect_to admin_customer_path(customer.id)
   end
 end
