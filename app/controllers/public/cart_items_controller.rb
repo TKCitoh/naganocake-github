7ffa6c3eba242ddd6f107ae7272f052695ef13a5
TKCitoh <tkci10.pooh@gmail.com>
2022-11-26 09:31:20 +0000

    enum実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+    @sum = 0
   end
 
   def update
@@ -9,9 +11,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    current_user.books.destroy_all
   end
 
   def create
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      same_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      same_cart_item.amount = same_cart_item.amount + params[:cart_item][:amount].to_i
+      same_cart_item.update(amount: same_cart_item.amount)
+    else
+      @cart_iten = CartItem.new
+    end
   end
 
   private
