7267df5bef6d81878cdebd1de04885c1f506aa7c
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-11-29 11:48:24 +0000

    管理者画面レイアウト

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customers.all
+    @customers = Customer.all
   end
 
   def show
@@ -14,6 +14,12 @@ class Admin::CustomersController < ApplicationController
   def update
     customer = Customer.find(params[:id])
     customer.update(customer_params)
-  　redirect_to admin_customer_path(customer.id)
+    redirect_to admin_customer_path(customer.id)
+  end
+
+  private
+  # ストロングパラメータ
+  def customer_params
+    params.require(:customer).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:is_deleted)
   end
 end
