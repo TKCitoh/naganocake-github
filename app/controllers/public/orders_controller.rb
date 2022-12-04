7dffd3d79928b8ff415fc2231521dfaf39b2ccd1
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-12-04 14:12:00 +0000

    ECサイト完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,6 +7,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @cart_items = CartItem.all
     @sum = 0
+    @order.shipping_cost = 500
     if params[:order][:address_select] == "ご自身の住所"
        @order.postal_code = current_customer.postal_code
        @order.address = current_customer.address
@@ -24,8 +25,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    order = Order.find(params[:id])
-    order.update(order_params)
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    current_customer.cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id #商品idの代入
+      @order_detail.amount = cart_item.amount #個数の代入
+      @order_detail.price = cart_item.item.with_tax_price #価格の代入
+      @order_detail.order_id =  @order.id #注文idの代入
+      @order_detail.save
+    end
+    current_customer.cart_items.destroy_all
     redirect_to complete_public_orders_path
   end
 
@@ -35,10 +46,12 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @sum = 0
+    @order_details = @order.order_details #特定した@orderからorder_detailsを全てとる
   end
 
   private
   def order_params
-  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_amount, :shipping_cost)
   end
 end
