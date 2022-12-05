68e08397063fc7e9e603ce84ab4a0aaee7eca3e7
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-12-05 09:47:46 +0000

    final commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @sum = 0
+    @order_details = @order.order_details
+    @order.shipping_cost = 500
   end
 
   def update
-    
+
   end
 end
